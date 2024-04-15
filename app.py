aebee6dad43cd1752f5f499bbdcd0f0340e9d799
boris83418 <boris83418@gmail.com>
2024-04-15 11:22:06 +0800

    upload first version

---
+++ app.py
@@ -0,0 +1,166 @@
+from flask import Flask, render_template, request
+import twstock
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
+from mplfinance.original_flavor import candlestick_ochl
+import plotly.graph_objs as go
+import requests
+from bs4 import BeautifulSoup
+
+
+
+# 畫K線圖
+def generate_k_line_plot(data):
+    fig = go.Figure(data=[go.Candlestick(x=data['年月'],
+                                         open=data['開盤價'],
+                                         high=data['最高價'],
+                                         low=data['最低價'],
+                                         close=data['收盤價'],
+                                         increasing=dict(line=dict(color='red')),
+                                         decreasing=dict(line=dict(color='green')))])
+    fig.update_layout(xaxis_rangeslider_visible=False)
+    k_line_plot = fig.to_html(full_html=False)
+    return k_line_plot
+
+# 算出月營收
+def get_monthly_revenue(stock_number):
+    # 構建查詢股票營收的 URL
+    url = f"https://goodinfo.tw/tw/ShowSaleMonChart.asp?STOCK_ID={stock_number}"
+    
+    # 發送 GET 請求獲取網頁內容
+    response = requests.get(url)
+    
+    # 如果請求成功
+    if response.status_code == 200:
+        # 使用 BeautifulSoup 解析 HTML 內容
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # 找到包含營收數據的表格
+        table = soup.find('div', id='divSaleMonChartDetail')
+        
+        # 創建一個空的列表來存儲有效的數據
+        data = []
+        
+        # 遍歷表格的每一行
+        for row in table.find_all('tr'):
+            # 獲取每一行的所有列
+            cols = row.find_all('td')
+            
+            # 將每一列的文本內容去除空白字符後存入列表
+            cols = [col.text.strip() for col in cols]
+            
+            if len(cols) == 17:
+                # 將月份拆分為年份和月份
+                    year_month = cols[0].split('/')
+                    year = year_month[0]
+                    month = year_month[1]
+                    # 添加年份和月份到列表中
+                    data.append([year, month] + cols[1:])
+        
+        # 定義 DataFrame 的列名
+        columns = ["年份", "月份", "開盤", "收盤", "最高", "最低", 
+                   "漲跌", "漲跌(%)", "單月營收","單月月增",
+                   "單月年增", "累計營收", "累計年增", "合併營業收入_單月營收",
+                   "合併營業收入_月增","合併營業收入_年增", "累計營收", "累計年增"]
+        
+        # 將數據列表轉換為 DataFrame
+        df = pd.DataFrame(data, columns=columns)
+        return df
+    else:
+        # 如果請求失敗，則打印錯誤信息並返回None
+        print(f"Failed to fetch data from {url}. Status code: {response.status_code}")
+        return None
+
+# 抓獲利情況   
+def get_profit(stock_number):
+    # 構建查詢股票營收的 URL
+    url = f"https://goodinfo.tw/tw/StockDetail.asp?STOCK_ID={stock_number}"
+    
+    # 發送 GET 請求獲取網頁內容
+    response = requests.get(url)
+    
+    # 如果請求成功
+    if response.status_code == 200:
+        # 使用 BeautifulSoup 解析 HTML 內容
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # 找到包含營收數據的表格
+        table = soup.find('div', id='FINANCE_INCOME_M')
+        
+        # 創建一個空的列表來存儲有效的數據
+        data = []   
+        
+        # 遍歷表格的每一行
+        for row in table.find_all('tr'):
+            # 獲取每一行的所有列
+            cols = row.find_all('td')
+            
+            # 將每一列的文本內容去除空白字符後存入列表
+            cols = [col.text.strip() for col in cols]
+            data.append(cols)
+
+  
+        # 定義 DataFrame 的列名
+        columns = ["年季", "營收(億)", "稅後淨利(億)", "毛利(%)", "營業利益(%)", "稅後淨利(%)", 
+                   "ROE(%)", "EPS(元)"]
+        data=data[5:]
+        
+        # 將數據列表轉換為 DataFrame
+        df = pd.DataFrame(data, columns=columns)
+        print(df)
+        return df
+    else:
+        # 如果請求失敗，則打印錯誤信息並返回None
+        print(f"Failed to fetch data from {url}. Status code: {response.status_code}")
+        return None
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+
+
+
+def index():
+    if request.method == 'POST':
+        stock_number = request.form['stock_number']
+        
+        # 獲取股票數據
+        stock = twstock.Stock(stock_number)
+        target_price = stock.fetch_from(2019, 10)
+        
+        #print(target_price)
+        # 對數據進行預處理和分組
+        grouped_data = {}
+        for row in target_price:
+            date = row[0]
+            year_month = (date.year, date.month)
+            if year_month not in grouped_data:
+                grouped_data[year_month] = {'開盤價': row[3], '最高價': row[4], '最低價': row[5], '收盤價': row[6]}
+            else:
+                grouped_data[year_month]['最高價'] = max(grouped_data[year_month]['最高價'], row[4])
+                grouped_data[year_month]['最低價'] = min(grouped_data[year_month]['最低價'], row[5])
+                grouped_data[year_month]['收盤價'] = row[6]
+        
+        # 轉換為 DataFrame，並添加年月
+        df = pd.DataFrame(grouped_data).T.reset_index()
+        df.columns = ['年份', '月份', '開盤價', '最高價', '最低價', '收盤價']
+        df['年月'] = pd.to_datetime(df['年份'].astype(str) + '-' + df['月份'].astype(str), format='%Y-%m')
+        
+        # 獲取股票月營收數據
+        financial_data = get_monthly_revenue(stock_number)
+        # 獲取股票淨利
+        profit_data = get_profit(stock_number)
+        
+        # 生成K線圖
+        k_line_plot = generate_k_line_plot(df[['年月','開盤價', '最高價', '最低價', '收盤價']])
+        
+        # 將股票數據及K財務都丟回前端
+        return render_template('index.html', stock_data=df, k_line_plot=k_line_plot,
+                               financial_data=financial_data, profit_data=profit_data)
+    
+    # 用戶未提交前都為none
+    return render_template('index.html', stock_data=None, k_line_plot=None,
+                           financial_data=None,profit_data=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
