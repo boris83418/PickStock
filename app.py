fcdb4576ff13c5a4ee3f92b3bec12ba103bd2959
boris83418 <boris83418@gmail.com>
2024-04-15 16:16:13 +0800

    update季度資料

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from mplfinance.original_flavor import candlestick_ochl
 import plotly.graph_objs as go
 import requests
 from bs4 import BeautifulSoup
-
-
+import numpy as np
 
 # 畫K線圖
 def generate_k_line_plot(data):
@@ -24,7 +23,10 @@ def generate_k_line_plot(data):
     return k_line_plot
 
 # 算出月營收
-def get_monthly_revenue(stock_number):
+# 算出季營收
+# 創建一個空的 DataFrame 來存儲季度營收
+
+def get_monthly_quartly_revenue(stock_number):
     # 構建查詢股票營收的 URL
     url = f"https://goodinfo.tw/tw/ShowSaleMonChart.asp?STOCK_ID={stock_number}"
     
@@ -66,11 +68,38 @@ def get_monthly_revenue(stock_number):
         
         # 將數據列表轉換為 DataFrame
         df = pd.DataFrame(data, columns=columns)
-        return df
+        
+        df["去年同期月營收"] = df.apply(lambda x: df[(df["年份"] == str(int(x["年份"]) - 1)) & (df["月份"] == x["月份"])]["單月營收"].iloc[0] if len(df[(df["年份"] == str(int(x["年份"]) - 1)) & (df["月份"] == x["月份"])]) > 0 else None, axis=1)
+        # 將字符串數據轉換為浮點數
+        df["去年同期月營收"] = df["去年同期月營收"].astype(float)
+        df["單月營收"] = df["單月營收"].astype(float)
+        # 將錯誤值變為 0    
+        df["YOY"] = np.where(pd.notnull(df["去年同期月營收"]) & pd.notnull(df["單月營收"]), ((df["單月營收"] - df["去年同期月營收"]) / df["去年同期月營收"]) * 100, 0)
+        df["YOY"] = df["YOY"].round(2)
+         # 將月營收添加到季度營收 DataFrame 中
+        # 將月份轉換為季度
+        df["季度"] = df["月份"].astype(int) // 4 + 1
+        
+        
+        quarterly_revenue_df = pd.DataFrame(columns=["年份", "季度"])
+        quarterly_revenue_df = pd.concat([quarterly_revenue_df, df.groupby(["年份", "季度"])["單月營收"].sum().reset_index()], ignore_index=True)
+        # 排序
+        quarterly_revenue_df = quarterly_revenue_df.sort_values(by=['年份', "季度"], ascending=[False,False])
+        quarterly_revenue_df["單月營收"]=quarterly_revenue_df["單月營收"].round(2)
+        quarterly_revenue_df["去年同季營收"]=quarterly_revenue_df.apply(lambda x: df[(df["年份"] == str(int(x["年份"]) - 1)) & (df["季度"] == x["季度"])]["單月營收"].sum() if len(df[(df["年份"] == str(int(x["年份"]) - 1)) & (df["季度"] == x["季度"])]) > 0 else None, axis=1)
+        quarterly_revenue_df["去年同季營收"]=quarterly_revenue_df["去年同季營收"].round(2)
+        # 將錯誤值變為 0    
+        quarterly_revenue_df["YOY"] = np.where(pd.notnull(quarterly_revenue_df["去年同季營收"]) & pd.notnull(quarterly_revenue_df["單月營收"]), ((quarterly_revenue_df["單月營收"] - quarterly_revenue_df["去年同季營收"]) / quarterly_revenue_df["去年同季營收"]) * 100, 0)
+        quarterly_revenue_df["YOY"] = quarterly_revenue_df["YOY"].round(2)
+        print(quarterly_revenue_df)
+        
+        return df,quarterly_revenue_df
     else:
         # 如果請求失敗，則打印錯誤信息並返回None
         print(f"Failed to fetch data from {url}. Status code: {response.status_code}")
         return None
+
+
 
 # 抓獲利情況   
 def get_profit(stock_number):
@@ -108,7 +137,6 @@ def get_profit(stock_number):
         
         # 將數據列表轉換為 DataFrame
         df = pd.DataFrame(data, columns=columns)
-        print(df)
         return df
     else:
         # 如果請求失敗，則打印錯誤信息並返回None
@@ -146,8 +174,8 @@ def index():
         df.columns = ['年份', '月份', '開盤價', '最高價', '最低價', '收盤價']
         df['年月'] = pd.to_datetime(df['年份'].astype(str) + '-' + df['月份'].astype(str), format='%Y-%m')
         
-        # 獲取股票月營收數據
-        financial_data = get_monthly_revenue(stock_number)
+        # 獲取股票月營收數據及季營收
+        financial_data, quarterly_revenue = get_monthly_quartly_revenue(stock_number)
         # 獲取股票淨利
         profit_data = get_profit(stock_number)
         
@@ -156,11 +184,11 @@ def index():
         
         # 將股票數據及K財務都丟回前端
         return render_template('index.html', stock_data=df, k_line_plot=k_line_plot,
-                               financial_data=financial_data, profit_data=profit_data)
+                               financial_data=financial_data, quarterly_revenue=quarterly_revenue, profit_data=profit_data)
     
     # 用戶未提交前都為none
     return render_template('index.html', stock_data=None, k_line_plot=None,
-                           financial_data=None,profit_data=None)
+                           financial_data=None, quarterly_revenue=None,profit_data=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
